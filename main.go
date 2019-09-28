c35891f9d672266b3e6168dfee2b5fa7f6112ff5
Ryuichi Hoshi <hoshi.ryuichi@gmail.com>
2019-09-28 16:05:42 +0900

    wip

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var DefaultDockerImage = "hoshir/katago-v1.2-cuda10.0-linux-x64"
+var MountDir = "/data"
+
+var (
+	configFile = flag.String("config", "", "A path to the config file")
+	image      = flag.String("image", DefaultDockerImage, "A docker image name")
+	modelFile  = flag.String("model", "", "A path to the model file")
+)
+
+func getPathInDocker(base, path string) (string, error) {
+	if strings.Contains(path, "..") || strings.HasPrefix(path, "/") {
+		return "", fmt.Errorf("invalid path")
+	}
+
+	if strings.HasPrefix(path, "./") {
+		path = path[2:]
+	}
+
+	return fmt.Sprintf("%s/%s", MountDir, path), nil
+}
+
+func buildDockerCommand() string {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	modelFilePath, err := getPathInDocker(currentDir, *modelFile)
+	if err != nil {
+		panic(err)
+	}
+
+	configFilePath, err := getPathInDocker(currentDir, *configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	dockerArgs := fmt.Sprintf("run -it --rm --runtime nvidia -v %s:%s", currentDir, MountDir)
+	katagoArg := fmt.Sprintf("gtp -model %s -config %s -override-version 0.17", modelFilePath, configFilePath)
+
+	return fmt.Sprintf("%s %s %s", dockerArgs, *image, katagoArg)
+}
+
+func checkArgs() error {
+	flag.Parse()
+
+	// Check arguments
+	if modelFile == nil {
+		log.Fatalln("-model should not be empty")
+	}
+
+	if configFile == nil {
+		log.Fatalln("-config should not be empty")
+	}
+}
+
+func main() {
+	checkArgs()
+
+	dockerArgs := strings.Split(buildDockerCommand(), " ")
+	cmd := exec.Command("docker", dockerArgs...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal("Failed to start docker", err)
+	}
+
+	cmd.Wait()
+}
